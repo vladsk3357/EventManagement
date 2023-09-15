04e40d75b636960dbf483281b2f09715b36f6726
Vladyslav <ipz201_svo@student.ztu.edu.ua>
2023-09-15 21:12:36 +0300

    Add graphql api

---
+++ src/EventManagement.WebApi/GraphApi/Common/EmptyFieldResolver.cs
@@ -0,0 +1,9 @@
+﻿using GraphQL;
+using GraphQL.Resolvers;
+
+namespace EventManagement.WebApi.GraphApi.Common;
+
+internal class EmptyFieldResolver: IFieldResolver
+{
+    public ValueTask<object?> ResolveAsync(IResolveFieldContext context) => new(new { });
+}
---
+++ src/EventManagement.WebApi/GraphApi/Mutation.cs
@@ -0,0 +1,14 @@
+﻿using GraphQL.Types;
+
+namespace EventManagement.WebApi.GraphApi;
+
+internal class Mutation : ObjectGraphType<object>
+{
+    public Mutation()
+    {
+        Name = "Mutation";
+
+        Field<StringGraphType>("Ping")
+            .Resolve(context => "Pong");
+    }
+}
